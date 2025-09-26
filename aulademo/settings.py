fc1a8727e15dc886a1d6c1db125a39d138b447c9
William Mattson Kheel Gomes Araújo <william.mattson02@gmail.com>
2025-09-26 09:05:22 -0400

    commit create core

--- aulademo/settings.py
+++ aulademo/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
+    'core.apps.CoreConfig',
     'rest_framework',
     'django_filters',
     'corsheaders'
---
+++ core/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+class ModelBase(models.Model):
+    id = models.BigAutoField(
+        db_column='id',
+        null=False,
+        primary_key=True,
+    )
+
+    created_at = models.DateTimeField(
+        db_column='created_at',
+        auto_now_add=True,
+        null=False,
+    )
+
+    modified_at = models.DateTimeField(
+        db_column='modified_at',
+        auto_now=True,
+        null=False,
+    )
+    active = models.BooleanField(  # Se está ativo ou não
+        db_column='cs_active',
+        default=True,
+        null=False,
+    )
+
+    class Meta:
+        abstract = True
+        managed = False
+
+
+class Author(ModelBase):
+    name = models.CharField(
+        db_column='tx_name',
+        max_length=255,
+        null=False,
+        blank=False,
+    )
+
+    def __str__(self):
+        return f'{self.id} - {self.name}'  # Quando for listado exibirá id e nome da pessoa
+
+    class Meta:
+        db_table = 'author'
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
