6dc7e1648224bc6131344ac2d5ae5f2d26ee5b6a
William Mattson Kheel Gomes Araújo <william.mattson02@gmail.com>
2025-09-26 08:47:44 -0400

    commit change password

--- aulademo/settings.py
+++ aulademo/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.environ.get('DB_NAME', 'aulademo'),
         'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'fpf2023'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', '123'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
